b668efb6bb513959f05d32ae51db585847f0bc45
danielaalineri <danielalineri@gmail.com>
2025-06-23 16:22:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/projeto/demo/services/userService.java
@@ -0,0 +1,33 @@
+package com.projeto.demo.services;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.projeto.demo.entities.User;
+import com.projeto.demo.repositories.UserRepository;
+
+
+
+@Service
+public class userService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+
+}
